69f89896ddb4916b03f9d3d76d08f574fdf491ce
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-11 17:32:17 -0400

    Face recognition

--- app/vision_service.py
+++ app/vision_service.py
@@ -17,22 +17,31 @@ def vision_client(creds_filepath=CREDS_FILEPATH):
     #print("VISION CLIENT:", type(client))
     return client
 
-def recognize_text(client, img_filepath):
+def local_image(img_filepath):
     with io.open(img_filepath, "rb") as img_file:
         img_contents = img_file.read()
-    img = types.Image(content=img_contents) #> <class 'google.cloud.vision_v1.types.Image'>
+    img = types.Image(content=img_contents)
+    return img
+
+def recognize_text(client, img_filepath):
+    img = local_image(img_filepath)
     response = client.text_detection(image=img) #> <class 'google.cloud.vision_v1.types.AnnotateImageResponse'>
-    #return response
     texts = response.text_annotations
     text = texts[0].description
     return text
 
-if __name__ == "__main__":
+def recognize_face(client, img_filepath):
+    img = local_image(img_filepath)
+    response = client.face_detection(image=img) #> <class 'google.cloud.vision_v1.types.AnnotateImageResponse'>
+    faces = response.face_annotations
+    face = faces[0]
+    return face
 
-    client = vision_client()
+if __name__ == "__main__":
 
     names = ["matthew", "michael", "michelle", "sally"] # TODO: get all files in img dir
     print("NAMES:", names)
+
     name = input("Select a name (e.g. 'sally'): ")
     if name not in names:
         name = random.choice(names)
@@ -41,5 +50,35 @@ if __name__ == "__main__":
     img_filepath = os.path.join(os.path.dirname(__file__), "..", "img", f"{name}.png")
     print("IMG FILE:", os.path.isfile(img_filepath), os.path.abspath(img_filepath))
 
-    response_text = recognize_text(client, img_filepath)
-    print(response_text)
+    client = vision_client()
+
+    print("----------------")
+    print("TEXT RECOGNITION...")
+    print("----------------")
+
+    license_text = recognize_text(client, img_filepath)
+    print(license_text)
+
+    print("----------------")
+    print("FACIAL RECOGNITION...")
+    print("----------------")
+
+    face = recognize_face(client, img_filepath)
+
+    print("CONFIDENCE:", face.detection_confidence, face.landmarking_confidence)
+    print("ANGLES:", face.roll_angle, face.pan_angle, face.tilt_angle)
+    print("ASPECTS:")
+    print("... UNDEREXPOSED:", face.under_exposed_likelihood)
+    print("... BLURRED:", face.blurred_likelihood)
+    print("... HEADWEAR:", face.headwear_likelihood)
+
+    print("EMOTIONS:")
+    print("... JOY:", face.joy_likelihood)
+    print("... SORROW:", face.sorrow_likelihood)
+    print("... ANGER:", face.anger_likelihood)
+    print("... SURPRISE:", face.surprise_likelihood)
+
+    print("LANDMARKS:")
+    for landmark in face.landmarks:
+        #print("...", landmark.type) # for some reason looks like "LEFT_EYE" but evaluates as 1, 2, 3, etc.
+        print(landmark)
--- test/vision_test.py
+++ test/vision_test.py
@@ -2,8 +2,8 @@ import os
 
 from dotenv import load_dotenv
 import pytest
-from google.cloud.vision_v1 import ImageAnnotatorClient, types
-from google.cloud.vision_v1.types import AnnotateImageResponse
+from google.cloud.vision_v1 import ImageAnnotatorClient
+from google.cloud.vision_v1.types import Image, AnnotateImageResponse, FaceAnnotation
 
 from app.vision_service import vision_client, recognize_text
 
@@ -16,16 +16,21 @@ def test_vision_client():
     mock_client = vision_client(mock_creds_filepath)
     assert isinstance(mock_client, ImageAnnotatorClient)
     assert "text_detection" in dir(mock_client)
+    assert "face_detection" in dir(mock_client)
+
+def test_local_image():
+    image_filepath = os.path.join(os.path.dirname(__file__), "..", "img", "sally.png")
+    assert isinstance(local_image(image_filepath), Image)
 
 @pytest.mark.skipif(CI, reason="requires real credentials, which aren't on the CI server")
 def test_text_recognition():
     client = vision_client()
     image_filepath = os.path.join(os.path.dirname(__file__), "..", "img", "sally.png")
-    image_text = "NEW YORK STATE\nCommission\nof Motor Vehicles\nDRIVER LICENSE\nID: 000 000 000\nCLASS DM\nSAMPLE,SALLY\n1010 ANYPLACE ST\nYOURCITY NY 12121\nDOB: 07-18-83\nSEX: F EYES: BL HT: 5-09\nE: NONE\nR: NONE\nISSUED: 07-18-05 EXPIRES: 07-18-13\nSety Somple\nCELSIOR\n83145522\n"
+    assert recognize_text(client, image_filepath) == "NEW YORK STATE\nCommission\nof Motor Vehicles\nDRIVER LICENSE\nID: 000 000 000\nCLASS DM\nSAMPLE,SALLY\n1010 ANYPLACE ST\nYOURCITY NY 12121\nDOB: 07-18-83\nSEX: F EYES: BL HT: 5-09\nE: NONE\nR: NONE\nISSUED: 07-18-05 EXPIRES: 07-18-13\nSety Somple\nCELSIOR\n83145522\n"
 
-    #response = recognize_text(client, image_filepath)
-    #assert isinstance(response, AnnotateImageResponse)
-    #assert response.text_annotations[0].locale == "en"
-    #assert response.text_annotations[0].description == expected_text
-
-    assert recognize_text(client, image_filepath) == image_text
+@pytest.mark.skipif(CI, reason="requires real credentials, which aren't on the CI server")
+def test_face_recognition():
+    client = vision_client()
+    image_filepath = os.path.join(os.path.dirname(__file__), "..", "img", "sally.png")
+    response = recognize_face(client, image_filepath)
+    assert isinstance(response, FaceAnnotation)

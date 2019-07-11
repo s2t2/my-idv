a006eaca4d443fc0110f17ad4366ebcb7ad5e682
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-11 00:45:49 -0400

    Refactor the client, run faces at random

--- app/vision_service.py
+++ app/vision_service.py
@@ -1,42 +1,52 @@
 import io # necessary?
 import os
 from pprint import pprint
+import random
 
 #from dotenv import load_dotenv
+from google.oauth2 import service_account
 from google.cloud import vision
 from google.cloud.vision import types
 
-#from oauth2client.service_account import ServiceAccountCredentials
-from google.oauth2 import service_account
-
 #load_dotenv() # load implicit creds GOOGLE_APPLICATION_CREDENTIALS
 
-if __name__ == "__main__":
-    CREDS_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "auth", "credentials.json")
-    print("CREDS FILE:", os.path.isfile(CREDS_FILEPATH), os.path.abspath(CREDS_FILEPATH))
+CREDS_FILEPATH = os.path.join(os.path.dirname(__file__), "..", "auth", "credentials.json")
 
-    #client = vision.ImageAnnotatorClient()
+def vision_client(creds_filepath=CREDS_FILEPATH):
+    #print("CREDS FILE:", os.path.isfile(creds_filepath), os.path.abspath(creds_filepath))
 
-    #creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILEPATH)
     creds = service_account.Credentials.from_service_account_file(CREDS_FILEPATH)
-    print("CREDS", type(creds))
+    #print("CREDS", type(creds))
+
     client = vision.ImageAnnotatorClient(credentials=creds)
+    #print("VISION CLIENT:", type(client))
+    #pprint(dir(client))
+    #print("------------")
 
-    print("VISION CLIENT:", type(client))
-    pprint(dir(client))
-    print("------------")
+    return client
 
-    img_filepath = os.path.join(os.path.dirname(__file__), "..", "img", "sally.png")
+if __name__ == "__main__":
+
+    names = ["matthew", "michael", "michelle", "sally"] # TODO: get all files in img dir
+    print("NAMES:", names)
+
+    name = input("Select a name (e.g. 'sally'): ")
+    if name not in names:
+        name = random.choice(names)
+    print("NAME:", name)
+
+    img_filepath = os.path.join(os.path.dirname(__file__), "..", "img", f"{name}.png")
     print("IMG FILE:", os.path.isfile(img_filepath), os.path.abspath(img_filepath))
 
     with io.open(img_filepath, "rb") as image_file:
         content = image_file.read()
     img = types.Image(content=content) #> <class 'google.cloud.vision_v1.types.Image'>
-    print("IMG", type(img))
+    #print("IMG:", type(img))
 
+    client = vision_client()
     response = client.text_detection(image=img) #> <class 'google.cloud.vision_v1.types.AnnotateImageResponse'>
-    print("RESPONSE", type(response))
-    pprint(dir(response))
+    #print("RESPONSE", type(response))
+    #pprint(dir(response))
     print("------------")
 
     #breakpoint()
@@ -48,7 +58,7 @@ if __name__ == "__main__":
     #    #print("------------------")
 
     text = texts[0]
-    print(type(text)) #> <class 'google.cloud.vision_v1.types.EntityAnnotation'>
+    #print(type(text)) #> <class 'google.cloud.vision_v1.types.EntityAnnotation'>
     print(text.description)
     #> NEW YORK STATE
     #> Commission

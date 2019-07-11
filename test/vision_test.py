cbb3a39e744ef6b8c78f5abfef63d095bc420411
Mike Rossetti <s2t2@users.noreply.github.com>
2019-07-11 16:31:16 -0400

    Use a valid example private key string to bypass errors

--- test/vision_test.py
+++ test/vision_test.py
@@ -5,6 +5,6 @@ from google.cloud.vision_v1 import ImageAnnotatorClient
 from app.vision_service import vision_client
 
 def test_vision_client():
-    example_creds_filepath = os.path.join(os.path.dirname(__file__), "..", "auth", "example_creds.json")
+    example_creds_filepath = os.path.join(os.path.dirname(__file__), "..", "auth", "mock_creds.json")
     client = vision_client(example_creds_filepath)
     assert isinstance(client, ImageAnnotatorClient)
